853827672748acbafcf5bdbdcfff1baa2793c3de
Sasha Korendiuk <sasha.d1uka@gmail.com>
2019-03-13 22:42:04 +0200

    added ex5_1 solution

---
+++ HW_05/src/Task5_1.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task5_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter array size");
+        int size = input();
+        int[] array = createArray(size);
+        int[] arrayReverse = reverseArray(array);
+        System.out.println("Your array is: " + Arrays.toString(array));
+        System.out.println("Reverse of your array is: " + Arrays.toString(arrayReverse));
+    }
+
+    static int[] createArray(int size) {
+        int[] array = new int[size];
+        for (int counter = 0; counter < array.length; counter++) {
+            array[counter] = (int) (Math.random() * 100);
+        }
+        return array;
+    }
+
+    static int[] reverseArray(int[] array) {
+        int[] arrayReverse = new int[array.length];
+        int counter = 0;
+        for (int i = (array.length - 1); i >= 0; i--) {
+            arrayReverse[counter] = array[i];
+            counter++;
+        }
+        return arrayReverse;
+    }
+
+    static int input() {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        if (size < 2) {
+            System.out.println("Incorrect array size");
+            System.exit(0);
+        }
+        return size;
+    }
+}
