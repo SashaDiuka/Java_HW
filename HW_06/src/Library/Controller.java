e4dd7bbf44d8ba3d7e0101a40470d50496fe6628
Sasha Korendiuk <sasha.d1uka@gmail.com>
2019-04-20 20:02:24 +0300

    Refactored HW_06 solution to Hometask_09

---
+++ HW_06/src/Library/Controller.java
@@ -0,0 +1,59 @@
+package Library;
+
+public class Controller {
+    private View view = new View();
+
+    public Controller() {
+    }
+
+    public void performance() {
+        Books library = new Books(25);
+        Book stealRat = new Book(001,
+                "Steal Rat",
+                "Harry Harryson",
+                "Publisher&co",
+                1994,
+                685,
+                225);
+        library.addBook(stealRat);
+        Book myBook = new Book(002,
+                "Как приручить айтишника",
+                "Me",
+                "D1uka",
+                1995,
+                299,
+                255);
+        library.addBook(myBook);
+        Book diy = new Book(003,
+                "Do It Yourself",
+                "Me",
+                "Home Factory",
+                2019,
+                100500,
+                333);
+        library.addBook(diy);
+        Book lordOfTheRings = new Book(004,
+                "Lord of the rings",
+                "J.R. Tolkien",
+                "Any publisher",
+                1998,
+                925,
+                788);
+        library.addBook(lordOfTheRings);
+        Book evolution = new Book(005,
+                "Происхождение видов",
+                "Чарльз Дарвин",
+                "Джон Мюррей",
+                1859,
+                502,
+                100000);
+        library.addBook(evolution);
+        view.showResult(library);
+        view.showMessage(View.ENTER_PERCENT_TO_CHANGE);
+        library.reprice(Utilites.inputInt());
+        view.showMessage(View.ASK_AUTHOR);
+        view.showResult(library.findByAuthor(Utilites.inputString()));
+        view.showMessage(View.ASK_YEAR);
+        view.showResult(library.findSinceYear(Utilites.inputInt()));
+    }
+}
---
+++ HW_06/src/Library/View.java
@@ -0,0 +1,22 @@
+package Library;
+
+public class View {
+    static final String ASK_YEAR = "Enter year, please";
+    static final String ASK_AUTHOR = "Enter author, please";
+    static final String EMPTY_SEARCH_RESULT = "There is no book, which meets your criteria";
+    static final String FULL_LIBRARY = "Library is full";
+    static final String EMPTY_LIBRARY = "Library is empty";
+    static final String ENTER_PERCENT_TO_CHANGE = "Enter percent to change";
+
+    public void showResult(Books library) {
+        System.out.println(library.toString());
+    }
+
+    public void showResult(Book book) {
+        System.out.println(book.toString());
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+}
